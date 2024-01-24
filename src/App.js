9d517298d7550c7e26d9454176edea42863a7b1d
prahul25 <palrahulkumar2002@gmail.com>
2024-01-25 01:10:57 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+import { useEffect, useState } from "react";
 
 
 function App() {
+  const [isRunning , setIsRunning] = useState(false)
+  const [elapsedTime , setElapsedTime] = useState(0)
+
+  const formatTime = (secs) => {
+    const mins = Math.floor(secs/60)
+    let remainingSecs = secs % 60
+    return `${mins}:${remainingSecs < 10 ? "0" : ""}${remainingSecs}`
+  }
+  const toggleTime = () => {
+    setIsRunning((prevIsRunning) => !prevIsRunning)
+  }
+
+  const reset = () => {
+    setIsRunning(false)
+    setElapsedTime(0)
+  }
+
+  useEffect(()=>{
+    let intervalId
+
+    if(isRunning){
+      intervalId = setInterval(() =>{
+        setElapsedTime((prevElapsedTime)=> prevElapsedTime +1 )
+      },1000)
+    }else{
+      clearInterval(intervalId)
+    }
+    
+    return () => clearInterval(intervalId)
+  },[isRunning])
   return (
-    <div className="App">
-      
+    <div >
+      <h1>Stopwatch</h1>
+      <p>Time: {formatTime(elapsedTime)}</p>
+      <button onClick={toggleTime}>{isRunning ? "Stop" : "Start"}</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
